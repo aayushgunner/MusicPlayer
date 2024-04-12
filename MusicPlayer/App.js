a3553c23123597bb0b6e94da01bb914e1e12091c
aayushgunner <luitelaayush8@gmail.com>
2024-04-12 14:39:39 +0545

    Home screen

--- MusicPlayer/App.js
+++ MusicPlayer/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Image, ImageBackground,StyleSheet, Text, View } from 'react-native';
 export default function App() {
+  const bgimage = require('./assets/bg.jpg');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       <ImageBackground source={bgimage} resizeMode="cover" style={styles.backgroundImage}>
       <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -16,5 +17,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
