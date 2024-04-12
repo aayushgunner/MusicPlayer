c14c512bd85aa56979718f94606037ffe947da07
YujanSubedi <0yujan0@gmail.com>
2024-04-12 16:27:21 +0545

    navi

--- MusicPlayer/App.js
+++ MusicPlayer/App.js
@@ -1,29 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, ImageBackground,StyleSheet, Text, View } from 'react-native';
 import Navigation from './Navigation';
 
 export default function App() {
-  const bgimage = require('./assets/bg.jpg');
   return (
-    <View style={styles.container}>
-       <ImageBackground source={bgimage} resizeMode="cover" style={styles.backgroundImage}>
-      <StatusBar style="auto" />
-        	  <Navigation/>
-
-      </ImageBackground>
-    </View>
+	  <Navigation/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  backgroundImage: {
-    flex: 1,
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
